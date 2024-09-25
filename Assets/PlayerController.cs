272b0b41cf9f17ea54db54e1f9d1a56e7ff3c172
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-09-25 17:45:23 +0900

    Feat: 캐릭터 점프, 착지 애니메이션 구현

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,31 +4,55 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpForce;
-    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject armObject;
+
     private float gravityScale;
 
-    void Start()
+    private static int runHash = Animator.StringToHash("Run");
+    private static int jumpHash = Animator.StringToHash("Jump");
+    private static int fallHash = Animator.StringToHash("Fall");
+
+    private int curAniHash;
+
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        gravityScale = rb.gravityScale;
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        gravityScale = rigid.gravityScale;
+
+        armObject.SetActive(true);
     }
 
-    void Update()
+    private void Update()
     {
-        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        Run();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            Jump();
         }
+
+        AnimatorPlay();
+    }
+
+    private void Run()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            rb.gravityScale = 0;
-            rb.velocity = Vector2.zero;
+            rigid.gravityScale = 0;
+            rigid.velocity = Vector2.zero;
         }
     }
 
@@ -36,7 +60,36 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            rb.gravityScale = gravityScale;
+            rigid.gravityScale = gravityScale;
+        }
+    }
+
+    private void AnimatorPlay()
+    {
+        int checkAniHash;
+
+        if (rigid.velocity.y > 0.5f)
+        {
+            checkAniHash = jumpHash;
+            armObject.SetActive(false);
+        }
+
+        else if (rigid.velocity.y < -0.5f)
+        {
+            checkAniHash = fallHash;
+            armObject.SetActive(false);
+        }
+
+        else
+        {
+            checkAniHash = runHash;
+            armObject.SetActive(true);
+        }
+
+        if (curAniHash != checkAniHash)
+        {
+            curAniHash = checkAniHash;
+            animator.Play(curAniHash);
         }
     }
 }
