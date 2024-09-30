f68eb5764174c1984320dddf73c9a5e6cb0096e5
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-09-30 17:47:18 +0900

    docs: 스크립트 위치 변경 및 주석 수정

--- Assets/Scripts/ESC_Menu.cs
+++ Assets/Scripts/ESC_Menu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 public class ESC_Menu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
-    public Button resumeButton; // 계속하기
-    public Button restartButton; // 다시하기
-    public Button menuButton; // 메뉴로 돌아가기
+    public Button resumeButton; //계속하기
+    public Button restartButton; //다시하기
+    public Button menuButton; //메뉴로 돌아가기
     private bool isPaused = false; //게임이 멈췄는지 여부
 
     private void Start()
