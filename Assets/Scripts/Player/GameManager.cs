de4f864146e93ca10cc979284be3ea1ee2692572
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-09-30 23:46:22 +0900

    feat: gameManager 초기화 (재시작했을때)

--- Assets/Scripts/Player/GameManager.cs
+++ Assets/Scripts/Player/GameManager.cs
@@ -1,26 +1,31 @@
-using Cinemachine;
 using System.Collections;
+using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    [SerializeField] int playerHealth = 3;
+    //초기 설정값을 저장하는 변수
+    //얘를 안 해 주면 재시작했을 때 목숨이 이상하게 전달됨
+    [SerializeField] private int initialPlayerHealth = 3;
+
+    private int playerHealth;
     [SerializeField] private Image[] hearts;
 
-    public int totalPoint;
-    public int stagePoint;
+    public int totalPoint; //게임 전체에서 얻은 점수
+    //현재는 딱히 의미가 없지만 스테이지가 증가하면 마지막에 총 합 점수를 알 수 있음
+    public int stagePoint; //현재 스테이지에서 얻은 점수
 
     [SerializeField] PlayerController player;
     [SerializeField] public Animator playerAnimator;
     [SerializeField] GameObject playerObject;
     [SerializeField] TextMeshProUGUI scoreText;
 
-    private bool isShieldActive;
+    private bool isShieldActive; //쉴드 착용 여부
     private WaitForSeconds delay;
 
     private void Awake()
@@ -29,27 +34,64 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+
+            //새로운 씬이 로드될 때마다 OnSceneLoaded를 호출하도록 이벤트 등록
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
             Destroy(gameObject);
+            return;
         }
+
+        //게임 시작 할 때마다 (재시작 포함) 목숨을 초기값으로 설정함
+        playerHealth = initialPlayerHealth;
     }
 
     private void Start()
     {
-        player = FindObjectOfType<PlayerController>();
-
-        if (playerObject != null)
-        {
-            playerAnimator = playerObject.GetComponent<Animator>();
-        }
-
-        float animationLength = playerAnimator.GetCurrentAnimatorStateInfo(0).length;
-        delay = new WaitForSeconds(animationLength);
+        InitializeGameObjects(); //게임 오브젝트들 초기화
 
         UpdateScoreUI();
         UpdateHealthUI();
+    }
+
+    //씬이 새로 로드될 때마다 플레이어의 목숨을 초기값으로 설정
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        playerHealth = initialPlayerHealth;
+        InitializeGameObjects();
+        UpdateHealthUI();
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+
+    //게임에 필요한 오브젝트 및 컴포넌트들을 초기화
+    private void InitializeGameObjects()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+        if (player != null)
+        {
+            playerObject = player.gameObject;
+            playerAnimator = player.GetComponent<Animator>();
+        }
+
+        scoreText = FindObjectOfType<TextMeshProUGUI>();
+
+        //Image컴포넌트를 hearts 배열에 저장
+        hearts = GameObject.FindGameObjectsWithTag("Heart")
+            .Select(heart => heart.GetComponent<Image>())
+            .ToArray();
+
+        float animationLength = playerAnimator != null ? playerAnimator.GetCurrentAnimatorStateInfo(0).length : 0;
+        delay = new WaitForSeconds(animationLength);
     }
 
     public void TakeDamage()
@@ -57,14 +99,18 @@ public class GameManager : MonoBehaviour
         if (isShieldActive) //쉴드가 활성화되어 있다면 피해X
             return;
 
-        if (player.gameObject.layer == 7)
-            return;
-        
-        playerHealth--;
-        UpdateHealthUI();
-        PlayerDamaged();
+        if (playerHealth >= 0)
+        {
+            if (player.gameObject.layer == 7)
+                return;
 
-        if (playerHealth < 0)
+            SoundManager.Instance.PlayAttackSound();
+            playerHealth--;
+            UpdateHealthUI();
+            PlayerDamaged();
+        }
+        
+        else
         {
             Die();
         }
@@ -72,6 +118,7 @@ public class GameManager : MonoBehaviour
 
     private void Die()
     {
+        SoundManager.Instance.PlayGameOverSound();
         playerAnimator.SetTrigger("Die");
         StartCoroutine(HandlePlayerDeath());
     }
@@ -98,10 +145,15 @@ public class GameManager : MonoBehaviour
     private IEnumerator HandlePlayerDeath()
     {
         yield return delay;
-        playerObject.SetActive(false);
+
+        if (playerObject != null)
+        {
+            playerObject.SetActive(false);
+        }
+
+        SoundManager.Instance.StopBGM();
         SceneManager.LoadScene("GameOver");
     }
-
 
     public int GetPlayerHealth()
     {
@@ -112,14 +164,19 @@ public class GameManager : MonoBehaviour
     {
         stagePoint += points;
         totalPoint += points;
-        UpdateScoreUI(); ;
+        UpdateScoreUI();
     }
 
+    //점수 UI 업데이트
     public void UpdateScoreUI()
     {
-        scoreText.text = "Score: " + stagePoint;
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + stagePoint;
+        }
     }
 
+    //목숨 UI 업데이트
     private void UpdateHealthUI()
     {
         for (int i = 0; i < hearts.Length; i++)
