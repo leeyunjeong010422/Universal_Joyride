091a3032eff613bcda4d5302cb525e516cc41561
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-10-02 09:57:58 +0900

    fix: 맵 수정

--- Assets/Scripts/TextAnimator.cs
+++ Assets/Scripts/TextAnimator.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 
+//참고: https://ruyagames.tistory.com/m/23
+//inputString: 최종적으로 표시될 문장
+//duration : 문장이 모두 표시될때까지 걸리는 시간
+//ease : 변환시의 시간당 변화량 그래프를 설정
 public class TextAnimator : MonoBehaviour
 {
     private Text targetText;
